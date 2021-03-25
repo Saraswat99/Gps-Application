fbc5e432840f4e5dc4582bd176fd199b8e662a49
Madan <madangupta8899@gmail.com>
2021-03-25 13:07:52 +0530

    date formatter added

--- src/main/java/com/veichle/app/utils/DateTimeUtils.java
+++ src/main/java/com/veichle/app/utils/DateTimeUtils.java
@@ -6,17 +6,19 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public interface DateTimeUtils {
+    DateFormat DF_DDMMYYYY = new SimpleDateFormat("dd-mm-yyyy");
+    DateFormat DF_DDMMYYYY_HH = new SimpleDateFormat("dd-mm-yyyy HH");
+    DateFormat DF_DDMMYYYY_HHMM = new SimpleDateFormat("dd-mm-yyyy HH:mm");
+    DateFormat DF_DDMMYYYY_HHMMSS = new SimpleDateFormat("dd-mm-yyyy HH:mm:ss");
+
 
     public static String convertToString(Date date){
-        DateFormat dateFormat = new SimpleDateFormat("E, dd MMM yyyy HH:mm:ss z");
-        String strDate = dateFormat.format(date);
+        String strDate = DF_DDMMYYYY_HHMMSS.format(date);
         return  strDate;
     }
 
-
-
-    public static Date convertToDate(String sdate) throws ParseException {
-        Date date=new SimpleDateFormat("dd/MM/yyyy").parse(sdate);
+    public static Date convertToDate(String dataString) throws ParseException {
+        Date date=DF_DDMMYYYY.parse(dataString);
         return date;
     }
 }
